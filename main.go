0b8bb1f60e3b1244a0abffbc809d26e2b263759d
ckukadiya <contact.chintankukadiya@gmail.com>
2020-02-11 15:39:09 +0530

    Initial commit

---
+++ main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"github.com/gin-gonic/gin"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"go.mongodb.org/mongo-driver/mongo/readpref"
+	"log"
+)
+
+func GetClient() *mongo.Client {
+	clientOptions := options.Client().ApplyURI("mongodb://localhost:27017")
+	client, err := mongo.NewClient(clientOptions)
+	if err != nil {
+		log.Fatal(err)
+	}
+	err = client.Connect(context.Background())
+	if err != nil {
+		log.Fatal(err)
+	}
+	return client
+}
+
+func main() {
+	fmt.Print("JS")
+	r := gin.Default()
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+	c := GetClient()
+	err := c.Ping(context.Background(), readpref.Primary())
+	if err != nil {
+		log.Fatal("Couldn't connect to the database", err)
+	} else {
+		log.Println("Connected!")
+	}
+	r.Run()
+
+}
---
+++ modules/person/person.model.go
@@ -0,0 +1,8 @@
+package model
+
+type Person struct {
+	name   string
+	age    int
+	gender string
+	state  string
+}
